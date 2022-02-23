e366de39b486bb45300e18a5b6390f3090a8ac02
nebHailemariam <nebhailemariam@gmail.com>
2022-02-23 16:20:51 +0300

    feat: add event to base entity

--- SharedKernel/src/BaseEntity.cs
+++ SharedKernel/src/BaseEntity.cs
@@ -5,6 +5,6 @@ namespace FantasySoccerManagementSystem.SharedKernel
     public abstract class BaseEntity<TId>
     {
         public TId Id { get; set; }
-
+        public List<BaseDomainEvent> Events;
     }
 }
