ab613f47e32463268b3666f08bf1901bd4f6ce9a
nebHailemariam <nebhailemariam@gmail.com>
2022-02-25 14:56:25 +0300

    feat: add endpoints

--- FantasySoccerManagement/src/FantasySoccerManagement.API/Endpoints/Leagues/LeaguesController.cs
+++ FantasySoccerManagement/src/FantasySoccerManagement.API/Endpoints/Leagues/LeaguesController.cs
@@ -1,33 +1,40 @@
 using Microsoft.AspNetCore.Mvc;
 using FantasySoccerManagementSystem.SharedKernel.Interfaces;
 using FantasySoccerManagement.Core.Aggregate;
+using FantasySoccerManagement.Api.Dtos;
+using AutoMapper;
 
-namespace FantasySoccerManagement.Api.LeaguesEndpoints
+namespace FantasySoccerManagement.Api
 {
     [ApiController]
     [Route("api/[controller]")]
     public class LeaguesController : ControllerBase
     {
-        private readonly IReadRepository<League> _leagueRepository;
+        private readonly IMapper _mapper;
+        private readonly IRepository<League> _leagueRepository;
+        private readonly IReadRepository<League> _leagueCachedRepository;
 
-        public LeaguesController(IReadRepository<League> leagueRepository)
+        public LeaguesController(IMapper mapper, IReadRepository<League> leagueCachedRepository, IRepository<League> leagueRepository)
         {
+            _mapper = mapper;
+            _leagueCachedRepository = leagueCachedRepository;
             _leagueRepository = leagueRepository;
         }
 
         [HttpGet]
         public async Task<IActionResult> Get()
         {
-            var response = await _leagueRepository.ListAsync();
+            var response = await _leagueCachedRepository.ListAsync();
             return Ok(response);
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create()
+        public async Task<IActionResult> Create([FromBody] LeagueCreateDto leagueCreateDto)
         {
-            var response = await _leagueRepository.ListAsync();
+            var league = new League(Guid.NewGuid(), leagueCreateDto.Name);
+            var createLeague = await _leagueRepository.AddAsync(league);
 
-            return Ok(response);
+            return Ok(createLeague);
         }
     }
 }
