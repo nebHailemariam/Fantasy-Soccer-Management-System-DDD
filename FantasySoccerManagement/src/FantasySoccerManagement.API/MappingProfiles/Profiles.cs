7bd1e4128af2c501dd9cc7eb4e96a2a691937378
nebHailemariam <nebhailemariam@gmail.com>
2022-02-25 14:57:39 +0300

    feat: add auto mapper configuration file

---
+++ FantasySoccerManagement/src/FantasySoccerManagement.API/MappingProfiles/Profiles.cs
@@ -0,0 +1,13 @@
+using AutoMapper;
+using FantasySoccerManagement.Api.Dtos;
+using FantasySoccerManagement.Core.Aggregate;
+
+namespace FantasySoccerManagement.Profiles
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+        }
+    }
+}
