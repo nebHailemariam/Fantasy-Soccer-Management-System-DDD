adba5f96e206ed2e1098e74046b4129e510a8ab3
nebHailemariam <nebhailemariam@gmail.com>
2022-02-27 13:28:26 +0300

    bugs: fix date of birth value not assigned

--- FantasySoccerManagement/src/FantasySoccerManagement.Core/Aggregates/Player.cs
+++ FantasySoccerManagement/src/FantasySoccerManagement.Core/Aggregates/Player.cs
@@ -6,13 +6,14 @@ namespace FantasySoccerManagement.Core.Aggregate
 {
     public class Player : BaseEntity<Guid>
     {
-        public Player(Guid id, string firstName, string lastName, string country, Guid teamId)
+        public Player(Guid id, string firstName, string lastName, string country, DateTime dateOfBirth, Guid teamId)
         {
             Id = Guard.Against.Default(id, nameof(id));
             FirstName = Guard.Against.NullOrWhiteSpace(firstName, nameof(firstName));
             LastName = Guard.Against.NullOrWhiteSpace(lastName, nameof(lastName));
             Country = Guard.Against.InvalidCountry(country, nameof(country));
             Value = 1000000;
+            DateOfBirth = dateOfBirth;
             TeamId = Guard.Against.Default(teamId, nameof(teamId));
             CreatedAt = DateTime.UtcNow;
         }
