36931dba2063841a96d959ae903d4b5b5470954f
nebHailemariam <nebhailemariam@gmail.com>
2022-03-14 16:49:18 +0300

    feat: add data context

---
+++ FantasySoccerPublic/src/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using FantasySoccerPublic.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FantasySoccerPublic.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Player> Players { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
