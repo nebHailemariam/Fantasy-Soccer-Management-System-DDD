00ae7b790e3a0f1fa2703e109bfa29cc59c96eb5
nebHailemariam <nebhailemariam@gmail.com>
2022-04-07 23:11:14 +0300

    feat: add consumer class

---
+++ FantasySoccerPublic/src/Helpers/ConsumerBase.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using FantasySoccerPublic.Helpers.Messaging;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Rmq.Common
+{
+    public abstract class ConsumerBase : RabbitMqClientBase
+    {
+        private readonly IMediator _mediator;
+        private readonly ILogger<ConsumerBase> _logger;
+
+        public ConsumerBase(
+            IMediator mediator,
+            ConnectionFactory connectionFactory,
+            ILogger<ConsumerBase> consumerLogger,
+            ILogger<RabbitMqClientBase> logger) :
+            base(connectionFactory, logger)
+        {
+            _mediator = mediator;
+            _logger = consumerLogger;
+        }
+
+        protected virtual async Task OnEventReceived<T>(object sender, BasicDeliverEventArgs @event)
+        {
+            try
+            {
+                var body = Encoding.UTF8.GetString(@event.Body.ToArray());
+                var message = JsonSerializer.Deserialize<T>(body);
+                await _mediator.Send(message);
+                Channel.BasicAck(@event.DeliveryTag, false);
+            }
+            catch (Exception ex)
+            {
+                Channel.BasicNack(@event.DeliveryTag, false, true);
+                _logger.LogCritical(ex, "Error while retrieving message from queue.");
+            }
+        }
+    }
+}
